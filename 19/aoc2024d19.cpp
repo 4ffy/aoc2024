56eee220578a4f952c0df7a52ac8dff519ea4a3b
Cameron Norton <cameron.norton@gmail.com>
2024-12-19 01:10:12 -0800

    Day 19 C++: I'll admit, I basically copied this. (Part 2 done.)

--- 19/aoc2024d19.cpp
+++ 19/aoc2024d19.cpp
@@ -1,114 +1,15 @@
 #include <fstream>
-#include <memory>
 #include <optional>
 #include <print>
 #include <regex>
 #include <sstream>
-#include <stdexcept>
 #include <string>
 #include <string_view>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
-
-class Trie
-{
-  private:
-	bool m_leaf;
-	optional<unique_ptr<Trie>> m_b; // Black
-	optional<unique_ptr<Trie>> m_g; // Green
-	optional<unique_ptr<Trie>> m_r; // Red
-	optional<unique_ptr<Trie>> m_u; // blUe
-	optional<unique_ptr<Trie>> m_w; // White
-
-  public:
-	Trie() : m_leaf(false), m_b({}), m_g({}), m_r({}), m_u({}), m_w({}) {}
-
-	size_t size()
-	{
-		size_t sum = m_leaf ? 1 : 0;
-		if (m_b) {
-			sum += m_b.value()->size();
-		}
-		if (m_g) {
-			sum += m_g.value()->size();
-		}
-		if (m_r) {
-			sum += m_r.value()->size();
-		}
-		if (m_u) {
-			sum += m_u.value()->size();
-		}
-		if (m_w) {
-			sum += m_w.value()->size();
-		}
-		return sum;
-	}
-
-	bool insert(string_view item)
-	{
-		if (item.length() == 0) {
-			if (!m_leaf) {
-				m_leaf = true;
-				return true;
-			} else {
-				return false;
-			}
-		}
-		string_view next{item.data() + 1, item.size() - 1};
-		switch (item.at(0)) {
-		case 'b':
-			if (!m_b) {
-				m_b = make_unique<Trie>();
-			}
-			return m_b.value()->insert(next);
-		case 'g':
-			if (!m_g) {
-				m_g = make_unique<Trie>();
-			}
-			return m_g.value()->insert(next);
-		case 'r':
-			if (!m_r) {
-				m_r = make_unique<Trie>();
-			}
-			return m_r.value()->insert(next);
-		case 'u':
-			if (!m_u) {
-				m_u = make_unique<Trie>();
-			}
-			return m_u.value()->insert(next);
-		case 'w':
-			if (!m_w) {
-				m_w = make_unique<Trie>();
-			}
-			return m_w.value()->insert(next);
-		default:
-			throw invalid_argument("Invalid char.");
-		}
-	}
-
-	bool contains(string_view item)
-	{
-		if (item.length() == 0) {
-			return m_leaf;
-		}
-		string_view next{item.data() + 1, item.size() - 1};
-		switch (item.at(0)) {
-		case 'b':
-			return m_b ? m_b.value()->contains(next) : false;
-		case 'g':
-			return m_g ? m_g.value()->contains(next) : false;
-		case 'r':
-			return m_r ? m_r.value()->contains(next) : false;
-		case 'u':
-			return m_u ? m_u.value()->contains(next) : false;
-		case 'w':
-			return m_w ? m_w.value()->contains(next) : false;
-		default:
-			throw invalid_argument("Invalid char.");
-		}
-	}
-};
 
 string read_file(string filename)
 {
@@ -146,19 +47,34 @@ optional<pair<vector<string>, vector<string>>> parse_data(string src)
 	return pair{patterns, designs};
 }
 
-bool validate_design(Trie &patterns, string_view design)
+bool validate_design(unordered_set<string_view> &patterns, string_view design)
 {
 	if (patterns.contains(design)) {
 		return true;
 	}
 	for (size_t i = 0; i < design.size(); ++i) {
-		string_view prefix{design.data(), design.size() - i};
-		string_view suffix{design.data() + design.size() - i, i};
+		string_view prefix = design.substr(0, design.size() - i);
+		string_view suffix = design.substr(design.size() - i, i);
 		if (patterns.contains(suffix) && validate_design(patterns, prefix)) {
 			return true;
 		}
 	}
 	return false;
+}
+
+long count_build_ways(unordered_set<string_view> patterns, string_view design)
+{
+	vector<long> cache(design.size() + 1, 0);
+	cache.at(0) = 1;
+	for (size_t i = 1; i <= design.size(); i++) {
+		for (auto pat : patterns) {
+			size_t length = pat.size();
+			if (i >= length && design.substr(i - length, length) == pat) {
+				cache.at(i) += cache.at(i - length);
+			}
+		}
+	}
+	return cache.at(design.size());
 }
 
 int main(int argc, char *argv[])
@@ -175,16 +91,24 @@ int main(int argc, char *argv[])
 	}
 	vector<string> patterns = std::move(data.value().first);
 	vector<string> designs = std::move(data.value().second);
-	Trie trie{};
+
+	unordered_set<string_view> pats{};
 	for (auto &p : patterns) {
-		trie.insert(p);
+		pats.insert(p);
 	}
-	int valid = 0;
+	long valid = 0;
 	for (auto &d : designs) {
-		if (validate_design(trie, d)) {
+		if (validate_design(pats, d)) {
 			++valid;
 		}
 	}
 	println("Valid designs: {}", valid);
+
+	long ways = 0;
+	for (auto &d : designs) {
+		ways += count_build_ways(pats, d);
+	}
+	println("Sum of ways(?): {}", ways);
+
 	return 0;
 }
